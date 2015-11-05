6cef46b0408dbfb235912877cf61ad74d1437de9
hector.moreno <hector.moreno@UNA10D1PCW7006.unaq.edu.mx>
2015-11-05 15:07:28 -0600

    Se realizó la pantalla del listado de productos

--- Providencia/application/controllers/Welcome.php
+++ Providencia/application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+            $this->load->helper(array('form', 'url'));
+		//$this->load->view('welcome_message');
+            $this->load->view('header_view');
+            $this->load->view('inicio_view');
+            $this->load->view('footer_view');
 	}
 }
---
+++ Providencia/application/views/header_view.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+
+<!-- Mirrored from webapplayers.com/inspinia_admin-v2.3/dashboard_4.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 22 Sep 2015 16:37:40 GMT -->
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Dashboard v.4</title>
+
+    <link href="<?= base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>css/animate.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>css/style.css" rel="stylesheet">
+    <!-- Toastr style -->
+    <link href="<?= base_url(); ?>css/plugins/toastr/toastr.min.css" rel="stylesheet">
+
+</head>
+
+<body class="top-navigation">
+
+    <div id="wrapper">
+        <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom white-bg">
+        <nav class="navbar navbar-static-top" role="navigation">
+            <div class="navbar-header">
+                <button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                    <i class="fa fa-reorder"></i>
+                </button>
+                <a href="#" class="navbar-brand">Inspinia</a>
+            </div>
+            <div class="navbar-collapse collapse" id="navbar">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a aria-expanded="false" role="button" href="layouts.html"> Back to main Layout page</a>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" role="button" href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu item <span class="caret"></span></a>
+                        <ul role="menu" class="dropdown-menu">
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" role="button" href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu item <span class="caret"></span></a>
+                        <ul role="menu" class="dropdown-menu">
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" role="button" href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu item <span class="caret"></span></a>
+                        <ul role="menu" class="dropdown-menu">
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a aria-expanded="false" role="button" href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu item <span class="caret"></span></a>
+                        <ul role="menu" class="dropdown-menu">
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                            <li><a href="#">Menu item</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <ul class="nav navbar-top-links navbar-right">
+                    <li>
+                        <a href="login.html">
+                            <i class="fa fa-sign-out"></i> Log out
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </div>
